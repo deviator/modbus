fc203211d30a43c54af5f6e1430b540b1def9d3b
deviator <code.viator@gmail.com>
2017-10-13 02:42:08 +0300

    improve doc

--- source/modbus/connection.d
+++ source/modbus/connection.d
@@ -4,14 +4,20 @@ module modbus.connection;
 /// Connection for backends
 interface Connection
 {
-    /// Write data to connection
+    /++ Write data to connection
+
+        Returns:
+            writed data length
+     +/
     size_t write(const(void)[] data);
 
     /++ Read data from connection
+
         Params:
-        buffer = allocated buffer for reading
+            buffer = allocated buffer for reading
+
         Returns:
-        slice of buffer with readed data
+            slice of buffer with readed data
      +/
     void[] read(void[] buffer);
 }
