1437734cae4cd90b58fc7404446d3d2c608134b1
deviator <code.viator@gmail.com>
2018-06-07 00:07:15 +0300

    add modbus tcp slave server

--- source/modbus/facade.d
+++ source/modbus/facade.d
@@ -4,14 +4,14 @@ module modbus.facade;
 import modbus.backend;
 import modbus.protocol;
 
-public import std.datetime : Duration, dur, hnsecs, nsecs, msecs, seconds;
+public import std.datetime : Duration, dur, hnsecs, usecs, nsecs, msecs, seconds;
 
 import modbus.connection.tcp;
-import std.socket : Socket;
+import std.socket : Socket, SocketSet, TcpSocket, SocketShutdown;
 
 import modbus.connection.rtu;
 
-/// ModbusMaster with RTU backend
+/// Modbus master with RTU backend
 class ModbusRTUMaster : ModbusMaster
 {
 protected:
@@ -30,7 +30,7 @@ public:
     inout(SerialPort) port() inout @property { return spcom.port; }
 }
 
-/// ModbusSingleSlave with RTU backend
+/// Modbus slave with RTU backend
 class ModbusRTUSlave : ModbusSlave
 {
 protected:
@@ -49,7 +49,7 @@ public:
     inout(SerialPort) port() inout @property { return spcom.port; }
 }
 
-/// Modbus with TCP backend based on TcpSocket from std.socket
+/// Modbus master with TCP backend based on TcpSocket from std.socket
 class ModbusTCPMaster : ModbusMaster
 {
 protected:
@@ -57,34 +57,119 @@ protected:
 
 public:
     ///
-    this(Address addr, SpecRules sr=null)
+    this(Address addr, void delegate(Duration) sf=null, SpecRules sr=null)
     {
-        mtc = new MasterTcpConnection(addr);
+        mtc = new MasterTcpConnection(addr, sf);
         super(new TCP(sr), mtc);
     }
 
     ///
     inout(Socket) socket() inout @property { return mtc.socket; }
 
-    ~this() { mtc.socket.close(); }
+    ~this()
+    {
+        mtc.socket.shutdown(SocketShutdown.BOTH);
+        mtc.socket.close();
+    }
 }
 
-///
-//class ModbusTCPSlave : ModbusSlave
-//{
-//protected:
-//    SlaveTcpConnection mtc;
-//
-//public:
-//    ///
-//    this(ModbusSlaveModel mdl, Address addr, SpecRules sr=null)
-//    {
-//        mtc = new SlaveTcpConnection(addr);
-//        super(mdl, new TCP(sr), mtc);
-//    }
-//
-//    ///
-//    inout(TcpSocket) socket() inout @property { return mtc.socket; }
-//
-//    ~this() { mtc.socket.close(); }
-//}
+/// Modbus
+class ModbusTCPSlaveServer
+{
+protected:
+
+    import core.thread : Fiber;
+    import modbus.msleep : msleep;
+
+    ModbusSlaveModel model;
+    TCP be;
+    TcpSocket serv;
+    SocketSet ss;
+
+    void delegate(Duration) sleepFunc;
+
+    void sleep(Duration d)
+    {
+        if (sleepFunc !is null) sleepFunc(d);
+        else msleep(d);
+    }
+
+    static class MBS : Fiber
+    {
+        ModbusSlave mb;
+        SlaveTcpConnection con;
+
+        this(ModbusSlave mb, SlaveTcpConnection con)
+        {
+            this.mb = mb;
+            this.con = con;
+            super(&run);
+        }
+
+        void run() { while (true) mb.iterate; }
+    }
+
+    MBS[] slaves;
+    size_t maxConCount;
+
+public:
+
+    ///
+    this(ModbusSlaveModel mdl, Address addr, int acceptConQueueLen=16,
+         size_t maxConCount=128, void delegate(Duration) sf=null, SpecRules sr=null)
+    {
+        model = mdl;
+        be = new TCP(sr);
+        sleepFunc = sf;
+
+        serv = new TcpSocket;
+        serv.blocking = false;
+        serv.bind(addr);
+        serv.listen(acceptConQueueLen);
+        this.maxConCount = maxConCount;
+
+        ss = new SocketSet;
+    }
+
+    ///
+    void iterate()
+    {
+        ss.reset();
+        ss.add(serv);
+
+        while (Socket.select(ss, null, null, Duration.zero))
+        {
+            auto s = serv.accept;
+            if (slaves.length >= maxConCount)
+            {
+                s.shutdown(SocketShutdown.BOTH);
+                s.close();
+                continue;
+            }
+            auto con = new SlaveTcpConnection(s, sleepFunc);
+            slaves ~= new MBS(new ModbusSlave(model, be, con), con);
+            this.sleep(Duration.zero);
+        }
+
+        if (slaves.any!(a=>!a.con.isAlive))
+            slaves = slaves.filter!(a=>a.con.isAlive).array;
+
+        foreach (sl; slaves)
+        {
+            try sl.call;
+            catch (CloseTcpConnection)
+                sl.con.close();
+
+            this.sleep(Duration.zero);
+        }
+    }
+
+    ///
+    inout(Socket) socket() inout @property { return serv; }
+
+    ~this()
+    {
+        serv.shutdown(SocketShutdown.BOTH);
+        serv.close();
+    }
+}
