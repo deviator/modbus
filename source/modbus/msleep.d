7a5d12243d2984ff50b8207b03170e609ba71e9c
deviator <code.viator@gmail.com>
2018-06-07 00:05:45 +0300

    add

---
+++ source/modbus/msleep.d
@@ -0,0 +1,14 @@
+module modbus.msleep;
+
+import core.thread : Fiber, Thread;
+import std.datetime.stopwatch : Duration, StopWatch, AutoStart;
+
+void msleep(Duration d)
+{
+    if (auto f = Fiber.getThis)
+    {
+        const sw = StopWatch(AutoStart.yes);
+        do f.yield(); while (sw.peek < d);
+    }
+    else Thread.sleep(d);
+}
