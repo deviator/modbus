cfe0f92861c6e4c31c6f98b0b9913d526d40dd94
deviator <code.viator@gmail.com>
2018-10-26 02:12:36 +0300

    upd serialport

--- source/modbus/msleep.d
+++ source/modbus/msleep.d
@@ -1,14 +1,3 @@
 module modbus.msleep;
 
-import core.thread : Fiber, Thread;
-import std.datetime.stopwatch : Duration, StopWatch, AutoStart;
-
-void msleep()(Duration d)
-{
-    if (auto f = Fiber.getThis)
-    {
-        const sw = StopWatch(AutoStart.yes);
-        do f.yield(); while (sw.peek < d);
-    }
-    else Thread.sleep(d);
-}
+public import serialport.util : msleep;
